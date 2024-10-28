034883d5f490a6b83bb6486c4f7278adfa4e74cb
Sylvain Cachard <sylvain.cachard@sogilis.com>
2024-10-28 11:17:01 +0100

    refont handling blob message admin side

--- geodezic-app/scripts/admin.js
+++ geodezic-app/scripts/admin.js
@@ -10,6 +10,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Fonction pour gérer les messages de localisation
 function handleLocationMessage(data) {
     try {
+        if (data instanceof Blob) {
+            console.log("message is a BLOB")
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                data = event.target.result;
+            };
+        }
         console.log('data being parsed = ', data);
         const location = JSON.parse(data);
         // Accéder aux champs du JSON
@@ -30,16 +37,5 @@ function handleLocationMessage(data) {
 }
 
 socket.addEventListener('message', function(event) {
-    // Vérifiez si le message est un Blob
-    if (event.target.result instanceof Blob) {
-        console.log("message is a BLOB")
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            handleLocationMessage(event.target.result);
-        };
-        reader.readAsText(event.target.result);
-    } else {
-        console.log("message is not a BLOB")
-        handleLocationMessage(event.data);
-    }
+    handleLocationMessage(event.data);
 });
